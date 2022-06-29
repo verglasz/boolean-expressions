5cb723199214ad811cde62062227a8273abd21b9
Valerio Lomanto <valerio.lomanto@sns.it>
2022-06-29 14:36:32 +0200

    use property testing with generators

--- build.sbt
+++ build.sbt
@@ -7,6 +7,7 @@ val circeVersion = "0.14.2"
 libraryDependencies ++= Seq(
 	"org.scalactic" %% "scalactic" % "3.2.12",
 	"org.scalatest" %% "scalatest" % "3.2.12" % "test",
+  "org.scalatestplus" %% "scalacheck-1-16" % "3.2.12.0" % "test",
   "io.circe" %% "circe-core" % circeVersion,
   "io.circe" %% "circe-generic" % circeVersion,
   "io.circe" %% "circe-parser" % circeVersion
--- src/test/scala/SerDeSpec.scala
+++ src/test/scala/SerDeSpec.scala
@@ -1,20 +1,43 @@
 import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.propspec.AnyPropSpec
+
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks._
 
 import boolexpr._
 import io.circe.parser._
 import io.circe.syntax._
+import io.circe._
 
-class SerDeSpec extends AnyFunSpec {
-  describe("Serialization") {
-    it("should produce valid JSON") {
-      assert(parse(serialize(True)).isRight)
-    }
+
+
+class SerDeSpec extends AnyPropSpec {
+  val allExprTypes: BooleanExpression = And(Not(Variable("A")),Or(True,False));
+  val deepTree: BooleanExpression =
+    Not(Or(
+      And(Not(allExprTypes), Variable("B")),
+      Not(And(
+        Or(allExprTypes, Variable("A")),
+        And(
+          Or(False, Variable("X")),
+          And(True, allExprTypes)
+        )
+      ))
+    ))
+
+  property("Serialization produces valid JSON") {
+    assert(parse(serialize(True)).isRight)
+    assert(parse(serialize(allExprTypes)).isRight)
+    assert(parse(serialize(deepTree)).isRight)
   }
 
-  describe("Deserialization") {
-    it("should give an error for an empty string") {
-      assert(deserialize("").isLeft)
-    }
+  property("Deserialization gives a parsing error for invalid JSON") {
+    forAll ("input") { (input: String) => {
+      whenever(parse(input).isLeft) {
+        assert(deserialize(input).isLeft)
+        assert(deserialize(input).left.get.isLeft)
+      }
+    }}
   }
+
 }
 
