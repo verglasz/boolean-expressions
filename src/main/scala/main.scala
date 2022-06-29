04a4b0241e829fa6ae357848ceab050c719eb460
Valerio Lomanto <valerio.lomanto@sns.it>
2022-06-29 12:13:51 +0200

    switch de/serialization to custom codec

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,21 @@
+import boolexpr._
+import scala.io.StdIn.readLine
+import System.err.println as eprintln
+
 @main
 def main(): Unit = {
-  println("Hello world!")
+
+  val input = Iterator.continually(readLine).takeWhile(_ != null).mkString("\n")
+  deserialize(input) match {
+    case Left(err) => {
+      eprintln("Ihe input is not a valid serialized BooleanExpression:")
+      eprintln(err)
+    }
+    case Right(exp) => {
+      eprintln("Obtained expression:")
+      eprintln(exp)
+      println(serialize(exp))
+    }
+  }
 }
+
